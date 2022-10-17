2909b30ff4e1dab1a12b02b1b7b07e9139217654
Clóvis Rodrigues <cloviswrodrigues@gmail.com>
2022-10-17 01:17:14 -0300

    added animation on click button

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,5 +1,5 @@
-const btnPrev = document.querySelector('.js-btn-prev');
-const btnNext = document.querySelector('.js-btn-next');
+const btnPrev = document.querySelectorAll('.js-btn-prev');
+const btnNext = document.querySelectorAll('.js-btn-next');
 
 const testimonialsItem = document.querySelectorAll('.testimonials-item');
 let currentIndex = 0
@@ -7,20 +7,25 @@ let currentIndex = 0
 function swapTestimonials(navigation) {    
     let indexNew = currentIndex
     if (navigation == 'previous') {
-        index = currentIndex - 1
-        if (index <= 0) {
-            index = testimonialsItem.length - 1;
+        indexNew = currentIndex - 1
+        if (indexNew < 0) {
+            indexNew = testimonialsItem.length - 1;
         }
     }else if (navigation == 'next') {
-        index = currentIndex + 1
-        if (index >= testimonialsItem.length) {
-            index = 0
+        indexNew = currentIndex + 1
+        if (indexNew >= testimonialsItem.length) {
+            indexNew = 0;
         }
     }
- 
+
     testimonialsItem[currentIndex].classList.remove('activate');
+    testimonialsItem[currentIndex].classList.add('disabled');
+
+    testimonialsItem[indexNew].classList.remove('disabled');
     testimonialsItem[indexNew].classList.add('activate');
+
+    currentIndex = indexNew;
 }
 
-btnPrev.addEventListener('click', () => swapTestimonials('previous'))
-btnNext.addEventListener('click', () => swapTestimonials('next'))
+btnPrev.forEach((e) => e.addEventListener('click', () => swapTestimonials('previous')));
+btnNext.forEach((e) => e.addEventListener('click', () => swapTestimonials('previous')));
